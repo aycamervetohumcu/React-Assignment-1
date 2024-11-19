e4484fec6420c19f5165a4c36856a0b122856ea5
aycamervetohumcu <amucar06@gmail.com>
2024-11-19 23:26:08 +0100

    components are created, images are added

---
+++ my-react-app/src/Components/Profile.jsx
@@ -0,0 +1,12 @@
+import ProfilePic from "../assests/image1.jpg";
+
+function Profile() {
+return
+<>
+<img src={profilePic} alt="profile picture" />
+    <h2>MErve</h2>
+    <p>Hello I am 26 years old and I like swimming</p>
+    </>
+}
+
+export default Profile;
