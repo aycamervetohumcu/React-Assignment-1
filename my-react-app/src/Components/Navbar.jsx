acb35b100ac161a49429dc19f499bbde888e431c
aycamervetohumcu <amucar06@gmail.com>
2024-11-24 22:05:10 +0100

    Navbar is added and menu is written

---
+++ my-react-app/src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+function NAvbar() {
+    return (
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/gallery">Gallery</Link></li>
+                <li><Link to="/counter">Counter</Link></li>
+                <li><Link to="/about">About</Link></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
