64688489635d468054b7cd75972e482f986882b0
aycamervetohumcu <amucar06@gmail.com>
2024-11-24 21:59:09 +0100

    React Router and navbar is entegrated

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import Home from './pages/Home';
+import CounterPage from './pages/CounterPage';
+import About from './pages/About';
 import Gallery from "./Components/Gallery.jsx";
 
 function App() {
-  return(
-    <div>
-   <h1>Gallery</h1>
-   <Gallery />
-   </div>
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/counter" element={<CounterPage />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
